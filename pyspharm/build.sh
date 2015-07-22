16a3a66bf318dc08297603b787d5a06ed9232db5
Daniel Rothenberg <darothen@mit.edu>
2015-07-22 13:04:04 -0400

    Specify LDFLAGS on osx for gfortran

--- pyspharm/build.sh
+++ pyspharm/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-python setup.py install <<"EOF"
+# Modify flags for build on osx
+if [ `uname` == Darwin ]; then
+    export LDFLAGS="-m64 -Wall -g -undefined dynamic_lookup -bundle"
+fi
+
+$PYTHON setup.py install <<"EOF"
 yes
 EOF
